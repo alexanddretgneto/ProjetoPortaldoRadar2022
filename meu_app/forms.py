41dd3a37b32b98be26eb13c3db9b0533fdafa0a1
alexandre teixeira gomes neto <120527776+alexanddretgneto@users.noreply.github.com>
2024-03-10 16:36:36 -0400

    Delete meu_app directory

--- meu_app/forms.py
+++
@@ -1,9 +0,0 @@
-# forms.py
-
-from django import forms
-from .models import Resposta
-
-class RespostaForm(forms.ModelForm):
-    class Meta:
-        model = Resposta
-        fields = ['nome', 'telefone', 'endereco']
--- meu_app/models.py
+++
@@ -1,8 +0,0 @@
-# models.py
-
-from django.db import models
-
-class Resposta(models.Model):
-    nome = models.CharField(max_length=100)
-    telefone = models.CharField(max_length=20)
-    endereco = models.CharField(max_length=200)
